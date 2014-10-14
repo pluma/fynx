23343d033275229a5b276dc71f2e9bae1e5af73e
Alan Plum <me@pluma.io>
2014-10-14 20:57:33 +0200

    Added stopListeningToAll, allow passing objects to listenTo.

--- listener-methods.js
+++ listener-methods.js
@@ -1,16 +1,22 @@
 /*jshint browserify: true */
 'use strict';
-module.exports = {
+var listenerMethods = {
   listenTo: function (emitter, callback) {
+    var self = this;
+    if (!callback) {
+      return Object.keys(emitter).forEach(function (key) {
+        listenerMethods.listenTo.call(self, emitter[key], key);
+      });
+    }
     var fn = callback;
     if (typeof callback === 'string') {
       var key = callback.substr(0, 1).toUpperCase() + callback.substr(1);
-      fn = this[callback] || this['on' + key] || this['handle' + key];
+      fn = self[callback] || self['on' + key] || self['handle' + key];
       if (!fn) throw new Error('Unknown method: ' + callback);
     }
-    if (!this._subscriptions) this._subscriptions = [];
-    var unsubscribe = emitter.listen(fn, this);
-    this._subscriptions.push({
+    if (!self._subscriptions) self._subscriptions = [];
+    var unsubscribe = emitter.listen(fn, self);
+    self._subscriptions.push({
       emitter: emitter,
       unsubscribe: unsubscribe
     });
@@ -26,5 +32,13 @@ module.exports = {
       });
     }
     return result;
+  },
+  stopListeningToAll: function () {
+    if (!this._subscriptions) return;
+    while (this._subscriptions.length) {
+      var sub = this._subscriptions.pop();
+      sub.unsubscribe();
+    }
   }
 };
+module.exports = listenerMethods;
