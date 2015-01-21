c4d9e1fd17b08648912375d84625b51ead42d53f
Bryan Larsen <bryan@larsen.st>
2015-01-21 10:02:21 -0500

    add bower support
    - uses webpack to create dist/fynx.js and dist/fynx.min.js
    - create initial bower.json
    - AXN and Immutable/contrib/cursor are included into fynx.js, Immutable is an external dependency
    - fynx is not yet registered into bower

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+var webpack = require('webpack');
+
+var plugins = [
+  new webpack.DefinePlugin({
+    'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+  })
+];
+
+if (process.env.COMPRESS) {
+  plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compressor: {
+        warnings: false
+      }
+    })
+  );
+}
+
+module.exports = {
+
+  output: {
+    library: 'Fynx',
+    libraryTarget: 'var'
+  },
+
+  externals: {
+    'immutable': 'Immutable'
+  },
+
+  node: {
+    buffer: false
+  },
+
+  plugins: plugins
+};
