23752014fbdb6de1e825c52a836dac31189692ea
Alan Plum <me@pluma.io>
2015-07-21 09:55:55 +0200

    Moved index.js to src/

---
+++ src/index.js
@@ -0,0 +1,19 @@
+/*jshint browserify: true */
+'use strict';
+var axn = require('axn');
+var createCursorStore = require('./create-cursor-store');
+module.exports = {
+  createAction: axn,
+  createActions: require('./create-actions'),
+  createAsyncAction: axn.async,
+  createAsyncActions: require('./create-async-actions'),
+  createStore: createCursorStore,
+  createSimpleStore: require('./create-simple-store'),
+  createCursorStore: createCursorStore,
+  createRawStore: require('./create-raw-store'),
+  listenTo: require('./listento-mixin'),
+  listenToProp: require('./listento-prop-mixin'),
+  connect: require('./connect-mixin'),
+  connectProp: require('./connect-prop-mixin'),
+  connectVia: require('./connect-via-mixin')
+};
